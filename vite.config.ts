c65e9c64d3023b39bf9bc791d2662e6a847372d8
sivansankarrajal07 <sivansankarraja@gmail.com>
2026-05-01 13:38:47 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,8 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  // ADD THIS LINE BELOW
+  base: '/emi-saver/', 
   plugins: [react(), tailwindcss(), viteSingleFile()],
   resolve: {
     alias: {
